2ebfc4c35556c940bf10a1b12cb115c35e853fa3
Mpm319 <112341652+Mpm319@users.noreply.github.com>
2022-12-10 12:27:42 -0500

    5000

--- crypto_app/__init__.py
+++ crypto_app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
 
 if __name__=="__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=5000)
     #my_app=create_app()
     #my_app.run(debug=True)
     
